65aa68748066927259c0f2150e030242e15ae971
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-02 17:52:57 -0400

    Fix: agora o botao de login fica desabilitado se não tiver email ou senha de no minimo 6 caracteres

--- front-end/src/Pages/LoginPage.jsx
+++ front-end/src/Pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { joiResolver } from '@hookform/resolvers/joi';
 import Joi from 'joi';
@@ -6,6 +6,8 @@ import './LoginPage.css';
 
 function LoginPage() {
   const minPasswordLength = 6;
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const loginSchema = Joi.object({
     email: Joi.string().email({ tlds: { allow: false } }).required(),
@@ -31,6 +33,8 @@ function LoginPage() {
               id="email-input"
               data-testid="common_login__input-email"
               aria-invalid={ errors.email ? 'true' : 'false' }
+              onChange={ (e) => setEmail(e.target.value) }
+              value={ email }
             />
           </label>
         </div>
@@ -42,13 +46,20 @@ function LoginPage() {
               id="password-input"
               data-testid="common_login__input-password"
               type="password"
+              onChange={ (e) => setPassword(e.target.value) }
+              value={ password }
             />
           </label>
         </div>
         <button
           type="submit"
           data-testid="common_login__button-login"
-          disabled={ errors.email || errors.password }
+          disabled={
+            errors.email
+            || errors.password
+            || email.length <= 0
+            || password.length <= minPasswordLength - 1
+          }
           className="button-forms-login"
         >
           LOGIN
