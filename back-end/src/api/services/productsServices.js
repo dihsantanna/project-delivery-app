e4fb287d062e34655eac681b80f16e71aa986b14
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-03 11:23:13 -0300

    refac: add try/catch in  productsServices

--- back-end/src/api/services/productsServices.js
+++ back-end/src/api/services/productsServices.js
@@ -2,13 +2,21 @@ const { StatusCodes: code } = require('http-status-codes');
 const { User } = require('../../database/models');
 
 const getAll = async () => {
+  try {
   const users = await User.findAll();
   return { code: code.OK, message: users };
+  } catch (error) {
+    return { code: code.INTERNAL_SERVER_ERROR, message: error };
+  }
 };
 
 const getById = async (id) => {
-  const user = await User.findOne({ where: { id } });
-  return { code: code.OK, message: user };
+  try {
+    const user = await User.findOne({ where: { id } });
+    return { code: code.OK, message: user };
+  } catch (error) {
+    return { code: code.NOT_FOUND, message: error };
+  }
 };
 
 module.exports = {
