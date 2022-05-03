68f8e0ae1d63d3457fe258cee1df0cc0546b57da
vitovls <vitovls37@gmail.com>
2022-05-03 18:47:05 -0300

    feat: Implementando comunicação com o backend e tratanto erros de payloads errados

--- front-end/src/Pages/Register/index.js
+++ front-end/src/Pages/Register/index.js
@@ -1,9 +1,61 @@
-import React from 'react';
+import Axios from 'axios';
+import React, { useState } from 'react';
+import {useHistory} from 'react-router-dom'
 import './styles.css';
 
 export default function Register() {
-  function showErrorMessage() {
-    return 'enabled';
+  
+  const [errorMessage, setErrorMessage] = useState({
+    show: 'disabled',
+    message: '',
+  });
+
+  const [newUser, setNewUser] = useState({
+    username: '',
+    email: '',
+    password: ''
+  })
+
+  const {username, email, password} = newUser;
+
+  const history = useHistory()
+
+  function setUserPayloadByInput({target}) {
+    const {name, value} = target
+    
+    setNewUser((prevState) => {
+      return {
+        ...prevState,
+        [name]: value
+      }
+    })
+  }
+
+  function createUser() {
+    Axios.post('http://localhost:3001/register', {
+      name: username,
+      email: email,
+      password: password,
+    }).then((_res) => {
+      setErrorMessage({
+        show: 'disabled',
+        message: ''
+      })
+      history.push('/customer/products')
+    })
+      .catch((err) => {
+      if(err.response.data.message) {
+        setErrorMessage({
+          show: 'enabled',
+          message: err.response.data.message
+        })
+      } else {
+        setErrorMessage({
+          show: 'enabled',
+          message: err.response.data
+        })
+      }
+      });
   }
 
   return (
@@ -18,6 +70,9 @@ export default function Register() {
             type="text"
             id="name-input"
             placeholder="Seu Nome"
+            name='username'
+            onChange={(event) => setUserPayloadByInput(event)}
+            value={username}
           />
         </label>
         <label htmlFor="mail-input">
@@ -28,6 +83,9 @@ export default function Register() {
             type="text"
             id="mail-input"
             placeholder="seu-email@site.com.br"
+            name='email'
+            onChange={(event) => setUserPayloadByInput(event)}
+            value={email}
           />
         </label>
         <label htmlFor="password-input">
@@ -38,21 +96,25 @@ export default function Register() {
             type="password"
             id="password-input"
             placeholder="******"
+            name='password'
+            onChange={(event) => setUserPayloadByInput(event)}
+            value={password}
           />
         </label>
         <button
           className="register_button"
           data-testid="common_register__button-register"
           type="button"
+          onClick={ () => createUser() }
         >
           CADASTRAR
         </button>
       </section>
       <span
         data-testid="common_register__element-invalid_register"
-        className={ `register-alert__${showErrorMessage()}` }
+        className={ `register-alert__${errorMessage.show}` }
       >
-        Mensagem de Erro!
+        {errorMessage.message.toUpperCase()}
       </span>
     </section>
   );
