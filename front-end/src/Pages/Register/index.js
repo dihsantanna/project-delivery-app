01a3e03a2ba620f03f099b5d05c6aaadb8c4ef30
vitovls <vitovls37@gmail.com>
2022-05-03 15:51:55 -0300

    feat: Adicionando classes e CSS na pagina de registro

--- front-end/src/Pages/Register/index.js
+++ front-end/src/Pages/Register/index.js
@@ -1,7 +1,59 @@
 import React from 'react';
+import './styles.css';
 
 export default function Register() {
+  function showErrorMessage() {
+    return 'enabled';
+  }
+
   return (
-    <div>Register</div>
+    <section className="register-container">
+      <h1 className="headling-register">Cadastro</h1>
+      <section className="register-container-inputs">
+        <label htmlFor="name-input">
+          <p>Nome</p>
+          <input
+            className="register_inputs"
+            data-testid="common_register__input-name"
+            type="text"
+            id="name-input"
+            placeholder="Seu Nome"
+          />
+        </label>
+        <label htmlFor="mail-input">
+          <p>Email</p>
+          <input
+            className="register_inputs"
+            data-testid="common_register__input-email"
+            type="text"
+            id="mail-input"
+            placeholder="seu-email@site.com.br"
+          />
+        </label>
+        <label htmlFor="password-input">
+          <p>Senha</p>
+          <input
+            className="register_inputs"
+            data-testid="common_register__input-password"
+            type="password"
+            id="password-input"
+            placeholder="******"
+          />
+        </label>
+        <button
+          className="register_button"
+          data-testid="common_register__button-register"
+          type="button"
+        >
+          CADASTRAR
+        </button>
+      </section>
+      <span
+        data-testid="common_register__element-invalid_register"
+        className={ `register-alert__${showErrorMessage()}` }
+      >
+        Mensagem de Erro!
+      </span>
+    </section>
   );
 }
