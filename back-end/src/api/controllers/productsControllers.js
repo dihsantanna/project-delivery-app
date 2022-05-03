a32f534383c21ecab8ab7e3583fdb63d8796798d
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-03 11:24:21 -0300

    feat: productsControllers

---
+++ back-end/src/api/controllers/productsControllers.js
@@ -0,0 +1,16 @@
+const productsServices = require('../services/productsServices');
+
+const getAll = async (req, res) => {
+  const { code, message } = await productsServices.getAll();
+  return res.status(code).json(message);
+};
+
+const getById = async (req, res) => {
+  const { code, message } = await productsServices.getById(req.params.id);
+  return res.status(code).json(message);
+};
+
+module.exports = {
+  getAll,
+  getById,
+};
