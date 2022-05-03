e01177964a824f262e0eaa06a8c19c3084b06b8f
Vitor Lima <vitorliman@gmail.com>
2022-05-03 15:03:31 -0300

    Fix: refatora register service

--- back-end/src/api/services/register.service.js
+++ back-end/src/api/services/register.service.js
@@ -1,7 +1,6 @@
 const jwt = require('jsonwebtoken');
 const md5 = require('md5');
 const { readFileSync } = require('fs');
-const schemaRegister = require('../middlewares/register.validate');
 const { StatusCodes: Code } = require('http-status-codes');
 const { User } = require('../../database/models');
 
@@ -9,22 +8,23 @@ const secret = readFileSync('jwt.evaluation.key', 'utf8');
 
 const createUser = async (data) => {
   try {
-    const { error } = schemaRegister.validate(data);
-    if (error) return { status: Code.BAD_REQUEST, message: error.details[0].message };
     const { name, email, password } = data;
-    const userEmailExists = await User.findOne({ email });
-    const userNameExists = await User.findOne({ name });
+    const hash = md5(password);
+    const userEmailExists = await User.findOne({ where: { email } });
+    const userNameExists = await User.findOne({ where: { name } });
     if (userEmailExists || userNameExists) {
       return {
-        status: StatusCodes.BAD_REQUEST,
+        code: 409,
         message: 'Email or name already exists',
       };
     }
-    const hash = md5(password);
-    const user = await User.create({ name, email, password: hash });
-    return { code: Code.OK, message: { ...user, token: jwt.sign(user, secret) } };
-  } catch {
-    return { status: Code.INTERNAL_SERVER_ERROR, message: 'Internal server error' };
+    const role = 'customer';
+    const token = jwt.sign({ name, email, role }, secret, { expiresIn: '1d' });
+    const user = await User.create({ name, email, password: hash, role });
+    const newUser = { code: Code.CREATED, message: { user, token } };
+    return newUser;
+  } catch (error) {
+    return { code: Code.INTERNAL_SERVER_ERROR, message: error.message };
   }
 };
 
