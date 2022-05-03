edcac406226900bb10d24449f9fecf17a6bbb6e1
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-03 11:10:37 -0300

    feat: middleware validateToken

---
+++ back-end/src/api/middlewares/tokenValidate.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+const { StatusCodes: code } = require('http-status-codes');
+const { readFileSync } = require('fs');
+
+const { User } = require('../../database/models');
+
+const secret = readFileSync('jwt.evaluation.key', 'utf8');
+
+const validateToken = async (req, res, next) => {
+  const token = req.headers.authorization;
+  if (!token) {
+    return res.status(code.UNAUTHORIZED).json({ message: 'Token not found' });
+  }
+  try {
+    const decoded = jwt.verify(token, secret);
+    const user = await User.findOne({ where: { email: decoded.data } });
+    if (!user) {
+      return res
+        .status(code.UNAUTHORIZED)
+        .json({ message: 'User not found' });
+    }
+    req.user = user;
+    next();
+  } catch (err) {
+    return res.status(code.UNAUTHORIZED).json({ message: 'Expired or invalid token' });
+  }
+};
+
+module.exports = validateToken;
