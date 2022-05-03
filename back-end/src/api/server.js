e7f4f68e9add14dfdd470eb5f15d0679b74133cd
Vitor Lima <vitorliman@gmail.com>
2022-05-03 15:03:01 -0300

    Feat: add register na rota

--- back-end/src/api/server.js
+++ back-end/src/api/server.js
@@ -1,4 +1,5 @@
 const login = require('./routes/login.route');
+const register = require('./routes/register.route');
 
 const port = process.env.PORT || 3001;
 const app = require('./app');
@@ -7,3 +8,4 @@ app.listen(port);
 console.log(`Api rodando na porta ${port}`);
 
 app.use('/login', login);
+app.use('/register', register);
