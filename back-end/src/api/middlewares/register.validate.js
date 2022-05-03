3436d7ff9654560ea62b442a6501687dac0b853b
Vitor Lima <vitorliman@gmail.com>
2022-05-03 15:01:18 -0300

    Fix: refatora validateRegister

--- back-end/src/api/middlewares/register.validate.js
+++ back-end/src/api/middlewares/register.validate.js
@@ -1,9 +1,18 @@
 const Joi = require('joi');
+const { StatusCodes: Code } = require('http-status-codes');
 
 const schemaRegister = Joi.object({
-  name: Joi.string().max(12).required(),
+  name: Joi.string().min(12).required(),
   email: Joi.string().email().required(),
   password: Joi.string().min(6).required(),
-})
+});
 
-export default schemaRegister;
+const validateRegister = (req, res, next) => {
+  const { body } = req;
+  const { error } = schemaRegister.validate(body);
+  if (error) return res.status(Code.BAD_REQUEST).json({ error: error.message });
+
+  next();
+};
+
+module.exports = validateRegister;
