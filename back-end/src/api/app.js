1ac29c8cde9e685f0834177a476e996dbacb78e1
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-03 13:01:08 -0300

    refac: server

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
+const loginRoute = require('./routes/login.route');
 const productsRoute = require('./routes/products.route');
 
 const app = express();
 
 app.use(express.json());
 
+app.use('/login', loginRoute);
 app.use('/products', productsRoute);
 
 module.exports = app;
