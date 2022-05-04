7f56d0f0dd487306df982a6c1550f0ed5e19dba4
ronaldosenajr <ronaldosenajr@gmail.com>
2022-05-03 20:10:48 -0400

    Fix: rota "/" redireciona para a rota "/login"

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, useHistory } from 'react-router-dom';
+
 import LoginPage from './Pages/LoginPage';
 
 function App() {
+  const history = useHistory();
   return (
     <main>
       <Switch>
         <Route exact path="/login" component={ LoginPage } />
-        <Route exact path="/" component={ LoginPage } />
+        <Route exact path="/" >
+          {history.push('/login')}
+        </Route>
       </Switch>
     </main>
   );
