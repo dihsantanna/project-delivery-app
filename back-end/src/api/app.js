cf0f619c7c65f10cd74dbb6b37cfb8a9ea545a53
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-04 19:54:18 -0300

    feat: route sellers

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const loginRoute = require('./routes/login.route');
 const registerRoute = require('./routes/register.route');
 const productsRoute = require('./routes/products.route');
+const sellersRoute = require('./routes/sellers.route');
 
 const app = express();
 
@@ -13,5 +14,6 @@ app.use(cors());
 app.use('/login', loginRoute);
 app.use('/register', registerRoute);
 app.use('/products', productsRoute);
+app.use('/sellers', sellersRoute);
 
 module.exports = app;
