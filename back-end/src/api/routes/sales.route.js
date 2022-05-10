809344a5696365fa3c2cefa62cc9bf75d3721428
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-10 17:12:56 -0300

    feat: create routes in sales.routes

--- back-end/src/api/routes/sales.route.js
+++ back-end/src/api/routes/sales.route.js
@@ -6,5 +6,8 @@ const salesControllers = require('../controllers/sales.controllers');
 const salesRoute = Router();
 
 salesRoute.post('/', validateToken, salesControllers.create);
+salesRoute.get('/:id', validateToken, salesControllers.getById);
+salesRoute.get('/', validateToken, salesControllers.getByQuery);
+salesRoute.patch('/:id', validateToken, salesControllers.updateStatus);
 
 module.exports = salesRoute;
