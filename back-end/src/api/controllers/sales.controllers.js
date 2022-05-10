05e3f227a892e84af3b593a5f278e7b8123acc24
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-10 17:12:15 -0300

    feat: create functions in sales.controllers

--- back-end/src/api/controllers/sales.controllers.js
+++ back-end/src/api/controllers/sales.controllers.js
@@ -5,6 +5,25 @@ const create = async (req, res) => {
   return res.status(code).json(message);
 };
 
+const getByQuery = async (req, res) => {
+  const { query } = req;
+  const { code, message } = await salesServices.getByQuery(query);
+  return res.status(code).json(message);
+};
+
+const getById = async (req, res) => {
+  const { code, message } = await salesServices.getById(req.params.id);
+  return res.status(code).json(message);
+};
+
+const updateStatus = async (req, res) => {
+  const { code, message } = await salesServices.updateStatus(req.params.id, req.body.status);
+  return res.status(code).json(message);
+};
+
 module.exports = {
   create,
+  getByQuery,
+  getById,
+  updateStatus,
 };
