7b62f7fdd230859233aa04bcd911a28aad5776ee
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-10 17:02:16 -0300

    feat: create functions in sales.services

--- back-end/src/api/services/sales.services.js
+++ back-end/src/api/services/sales.services.js
@@ -1,6 +1,5 @@
 const { StatusCodes: code } = require('http-status-codes');
-const { Sale } = require('../../database/models');
-const { SaleProduct } = require('../../database/models');
+const { Sale, SaleProduct, Product, User } = require('../../database/models');
 
 const salesToPost = (saleId, products) => {
   const result = products.map((product) => ({
@@ -30,6 +29,81 @@ const create = async (data) => {
   }
 };
 
+const getById = async (id) => {
+  try {
+    const sales = await Sale.findAll({
+      where: { id },
+      include: [{ model: SaleProduct, as: 'sale_products' }],
+    });
+    return { code: code.OK, message: sales };
+  } catch (error) {
+    return { code: code.INTERNAL_SERVER_ERROR, message: error };
+  }
+};
+
+const getAllByUserId = async (userId) => {
+  try {
+    const sales = await Sale.findAll({
+      where: { userId },
+      include: [
+        { model: User, as: 'user_sale', attributes: ['name'] },
+        {
+          model: SaleProduct,
+          as: 'sale_products',
+          include: [{ model: Product, as: 'product_sale', attributes: ['name', 'price'] }],
+        },
+      ],
+    });
+    return { code: code.OK, message: sales };
+  } catch (error) {
+    return { code: code.INTERNAL_SERVER_ERROR, message: error };
+  }
+};
+
+const getAllBySelerId = async (sellerId) => {
+  try {
+    const sales = await Sale.findAll({
+      where: { sellerId },
+      include: [
+        { model: User, as: 'seller_sale', attributes: ['name'] },
+        {
+          model: SaleProduct,
+          as: 'sale_products',
+          include: [{ model: Product, as: 'product_sale' }],
+        },
+      ],
+    });
+    return { code: code.OK, message: sales };
+  } catch (error) {
+    return { code: code.INTERNAL_SERVER_ERROR, message: error };
+  }
+};
+
+const getByQuery = async (query) => {
+  if (query.userId) {
+    return getAllByUserId(query.userId);
+  }
+  if (query.sellerId) {
+    return getAllBySelerId(query.sellerId);
+  }
+};
+
+const updateStatus = async (id, status) => {
+  try {
+    const sale = await Sale.findOne({ where: { id } });
+    if (!sale) {
+      return { code: code.NOT_FOUND, message: 'Sale not found' };
+    }
+    const updatedSale = await sale.update({ status });
+    return { code: code.OK, message: updatedSale };
+  } catch (error) {
+    return { code: code.INTERNAL_SERVER_ERROR, message: error };
+  }
+};
+
 module.exports = {
   create,
+  getByQuery,
+  getById,
+  updateStatus,
 };
