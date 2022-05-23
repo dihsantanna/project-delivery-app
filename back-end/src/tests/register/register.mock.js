faf4ad51bb4e9c519f07e8d26f2fd21540a79740
Matheus Henrique Gonzaga <matheushg@pop-os.localdomain>
2022-05-23 15:52:09 -0300

    feat: create tests for register.services

---
+++ back-end/src/tests/register/register.mock.js
@@ -0,0 +1,19 @@
+const registerPayload = {
+  name: "Matheus Henrique",
+  email: "matheushg@gmail.com",
+  role: "customer",
+  password: 'e81502a921e78c4ddb017a555586664c',
+};
+
+const registerMock = {
+  id: 4,
+  name: "Matheus Henrique",
+  email: "matheushg@gmail.com",
+  role: "customer",
+  token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1lIjoiTWF0aGV1cyBIZW5yaXF1ZSIsImVtYWlsIjoibWF0aGV1c2hnQGdtYWlsLmNvbSIsInJvbGUiOiJjdXN0b21lciIsImlhdCI6MTY1MzMyOTc2MiwiZXhwIjoxNjUzNDE2MTYyfQ.8aibNbUgjkzGQ1e_HJW8ltVAUyv7piFdSJlzP43TJ3I"
+};
+
+module.exports = {
+  registerMock,
+  registerPayload,
+};
